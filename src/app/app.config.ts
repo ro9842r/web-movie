d91a22f91bc8d02dc225ab911c78370657cc9200
Rodrigo Motta <rodrigo.motta.ext@spms.min-saude.pt>
2025-07-22 19:53:39 -0300

    first commit + config project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { provideLottieOptions } from 'ngx-lottie';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideLottieOptions({
+      player: () => import('lottie-web'),
+    }),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
 };
